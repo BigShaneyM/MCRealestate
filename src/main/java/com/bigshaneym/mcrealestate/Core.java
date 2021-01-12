c725217d15312d30a38ea393c4a6c5d47e6d2f49
BigShaneyM <shanemcp05@gmail.com>
2021-01-12 17:41:36 -0500

    First initial commit to plugin development

---
+++ src/main/java/com/bigshaneym/mcrealestate/Core.java
@@ -0,0 +1,14 @@
+package com.bigshaneym.mcrealestate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+
+    public void onEnable() {
+        System.out.println("MCRealestate enabling");
+    }
+
+    public void onDisable() {
+        System.out.println("MCRealestate disabling");
+    }
+}
